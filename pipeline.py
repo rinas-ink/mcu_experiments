8fe1f07f180c70210d3ceb5fa94ae8d4074e734c
Gleb Ingman <igr2309@gmail.com>
2023-11-10 13:18:29 +0100

    Added RRE calculation and plotting of the LD embedding

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,5 @@
 from data import generate_array_of_swiss_rolls, get_control_vars, get_p
+import matplotlib.pyplot as plt
 import numpy as np
 import cvxpy
 
@@ -56,10 +57,11 @@ def get_eigen_decomposition(q):
     return sorted_eigenvectors, np.diag(sorted_eigenvalues)
 
 
-def reduce_dimension(eigenvectors, sigma):
-    m_ = 1
-    while np.sum(sigma[:, :m_]) < 0.9 * np.sum(sigma):
-        m_ += 1
+def reduce_dimension(eigenvectors, sigma):  # FIXME: add Otsu's threshold
+    # m_ = 1
+    # while np.sum(sigma[:, :m_]) < 0.9 * np.sum(sigma):
+    #     m_ += 1
+    m_ = 2
     return np.dot(eigenvectors[:, :m_], np.sqrt(sigma[:m_, :m_]))
 
 
@@ -72,20 +74,40 @@ def maximum_covariance_unfolding_regression(control_vars, response_matrix):
     response_matrix = center(response_matrix)
     response_matrix = scale(response_matrix)
 
-    k = 5
-    c = 1e6
+    k = 5  # FIXME
+    c = 1e5  # FIXME
+
     edges = construct_graph(response_matrix, k)
     q = solve_semidefinite_programming(control_vars, response_matrix, edges, c)
     u, s = get_eigen_decomposition(q)
     y_ = reduce_dimension(u, s)
     b = regress(y_, control_vars)
-    print(b)
+
+    return control_vars, response_matrix, y_, b
     
+
+def compute_rre(ld_embedding, reconstructed_y):
+    return np.median(np.linalg.norm(ld_embedding - reconstructed_y, axis=1) / np.linalg.norm(ld_embedding, axis=1))
+
+
+def plot_two_embeddings(ld_embedding, reconstructed_y):
+    n = ld_embedding.shape[0]
+    fig = plt.figure(figsize=(14, 14))
+    ld_plot = fig.add_subplot(1, 2, 1)
+    ld_plot.scatter(ld_embedding[:, 0], ld_embedding[:, 1], s=10, c=ld_embedding[:, 0], cmap=plt.cm.Spectral)
+    rec_plot = fig.add_subplot(1, 2, 2)
+    rec_plot.scatter(reconstructed_y[:, 0], reconstructed_y[:, 1], s=10, c=reconstructed_y[:, 0], cmap=plt.cm.Spectral)
+    plt.show()
+
 
 def main():
     control_vars = get_control_vars(get_p())
     response_matrix = generate_array_of_swiss_rolls(control_vars)
-    maximum_covariance_unfolding_regression(control_vars, response_matrix)
+    standardized_x, centered_y, ld_embedding, regression_matrix = maximum_covariance_unfolding_regression(control_vars, response_matrix)
+    reconstructed_y = np.dot(standardized_x, regression_matrix)
+
+    print(compute_rre(ld_embedding, reconstructed_y))
+    plot_two_embeddings(ld_embedding, reconstructed_y)
 
 
 if __name__ == '__main__':
