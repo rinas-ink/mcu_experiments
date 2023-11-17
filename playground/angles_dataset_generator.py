972081ff758156573724f579c9ec94e00bfbe4c1
Tatiana Berlenko <tatiana.berlenko@gmail.com>
2023-11-17 12:55:42 +0200

    add angles performing

---
+++ playground/angles_dataset_generator.py
@@ -0,0 +1,85 @@
+import random
+import numpy as np
+
+from playground.vizualize import plot_figure, init_viz, show, set_limits, change_view_point
+
+
+
+
+def get_p():
+    return 3
+
+
+def get_max_value():
+    return 50
+
+
+def get_min_value():
+    return 15
+
+
+def add_noise(A, B, C, noise_level=0.05):
+    A += np.random.normal(0, noise_level, A.shape)
+    B += np.random.normal(0, noise_level, B.shape)
+    C += np.random.normal(0, noise_level, C.shape)
+
+
+def create_plane(point, scale, plane, num=20):
+    axes = {'xy': (0, 1, 2), 'yz': (1, 2, 0), 'xz': (0, 2, 1)}
+    a_idx, b_idx, c_idx = axes[plane]
+
+    a = np.linspace(point[a_idx], point[a_idx] + scale[a_idx], num)
+    b = np.linspace(point[b_idx], point[b_idx] + scale[b_idx], num)
+    A, B = np.meshgrid(a, b)
+    C = np.full_like(A, point[c_idx])
+
+    add_noise(A, B, C)
+
+    planes = {'xy': (A, B, C), 'yz': (C, A, B), 'xz': (A, C, B)}
+    return planes[plane]
+
+
+def create_figure(point, scale):
+    return [create_plane(point, scale, 'xy'),
+            create_plane(point, scale, 'yz'),
+            create_plane(point, scale, 'xz')]
+
+
+def get_control_vars(n=3):
+    return [random.randint(get_min_value(), get_max_value()) for _ in range(n)]
+
+
+def main():
+    ax = init_viz()
+    initial_point = (0, 0, 0)
+    scale = get_control_vars(get_p())
+    planes = create_figure(initial_point, scale)
+    plot_figure(ax, planes)
+    set_limits(ax, 0, get_max_value())
+    change_view_point(ax)
+
+    show()
+
+
+def get_array_of_control_vars(p, n):
+    return [get_control_vars(p) for _ in range(n)]
+
+
+def get_flat_array_of_3d_data(x, y, z):
+    return np.column_stack((x, y, z)).reshape(-1)
+
+
+def get_array_of_figures(control_vars, num):
+    samples = []
+    for i in range(num):
+        initial_point = (0, 0, 0)
+        length, width, height = control_vars[i]
+        xy, yz, xz = create_figure(initial_point, scale=(length, width, height))
+        sample = get_flat_array_of_3d_data(xy, yz, xz)
+        samples.append(sample)
+    return samples
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ playground/vizualize.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import numpy as np
+def init_viz():
+    fig = plt.figure()
+    return fig.add_subplot(111, projection='3d')
+
+
+def plot_plane(ax, x, y, z, color):
+    ax.plot_surface(x, y, z, alpha=0.5, rstride=1, cstride=1, color=color)
+
+
+def plot_figure(ax, planes):
+    plot_plane(ax, *planes['xy'], color='g')
+    plot_plane(ax, *planes['yz'], color='b')
+    plot_plane(ax, *planes['xz'], color='r')
+
+def set_limits(ax, initial_point, scale):
+    ax.set_xlabel('X Axis')
+    ax.set_ylabel('Y Axis')
+    ax.set_zlabel('Z Axis')
+    ax.set_xlim([initial_point, scale])
+    ax.set_ylim([initial_point, scale])
+    ax.set_zlim([initial_point, scale])
+
+
+def change_view_point(ax):
+    ax.view_init(elev=20., azim=30)
+
+def set_axes_equal(ax):
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    y_range = abs(y_limits[1] - y_limits[0])
+    z_range = abs(z_limits[1] - z_limits[0])
+
+    x_middle = np.mean(x_limits)
+    y_middle = np.mean(y_limits)
+    z_middle = np.mean(z_limits)
+
+    plot_radius = 0.5*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
+def show():
+    plt.show()
